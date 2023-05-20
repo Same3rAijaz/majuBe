f270101079814b97437305b1c0e416199d44dd3d
Same3rAijaz <aijazsameer5@gmail.com>
2023-05-20 16:30:02 +0500

    updated

--- Controller/UserController/Signup.js
+++ Controller/UserController/Signup.js
@@ -1,6 +1,6 @@
 import UserModel from "../../Model/UserModel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import saveFileToFirebase from "../../Util/SaveImage.js";
 const Signup = async (req, res) => {
     try {
@@ -8,10 +8,12 @@ const Signup = async (req, res) => {
         if (req?.file) {
             picture = await saveFileToFirebase(req?.file)
         }
-        const user = new UserModel({ ...req.body, password: bcrypt.hash(req?.body?.password), picture }).save()
-        const token = jwt.sign(user._id, process.env.JWT_SECRET_KEY)
+        const password = await bcrypt.hash(req?.body?.password, 2)
+        const user = await new UserModel({ ...req.body, password, picture }).save()
+        const token = jwt.sign(user._id?.toString(), process.env.JWT_SECRET_KEY)
         return res.status(200).send({ message: "Success!", data: { user: user, token: token } })
     } catch (error) {
+        console.log(error?.message)
         return res.status(400).send({ message: "Bad Request!" })
     }
 }
